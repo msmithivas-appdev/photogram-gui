9473d13fdbc2217d2e6186f06ac0a26b341ab2da
msmithivas <msmithivas@gmail.com>
2023-03-06 22:05:42 +0000

    Photos detail page More work on photos detail page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,9 +3,26 @@ class PhotosController < ApplicationController
   def index
 
     matching_photos = Photo.all
-    @list_of_photos = matching_photos.order({ :id => :asc })
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
     render({ :template => "photo_templates/index.html.erb" })
   end
+
+  def show
+
+    @url_photo = params.fetch("path_photo")
+
+    matching_photos = Photo.where({ :id => url_photo })
+
+    @the_photo = matching_photos.at(0)
+
+    if @the_photo == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show.html.erb "})
+    end
+
+  end
+
 
 
 end
