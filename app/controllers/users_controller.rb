89b8ef6416f0f00d92dc67a9edce24aad9a54194
msmithivas <msmithivas@gmail.com>
2023-03-06 20:52:25 +0000

    Users page Added more code to populate the users profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,8 +9,17 @@ class UsersController < ApplicationController
 
   def show
 
-# @username = params("path_username")
+    @url_username = params.fetch("path_username")
 
+    matching_usernames = User.where({ :username => url_username })
+
+    @the_user = matching_usernames.at(0)
+
+    if @the_user == nil
+      redirect_to("/404")
+    else
+      render({ :template => "user_templates/show.html.erb "})
+    end
 
   end
 
